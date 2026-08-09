4dcbed7d31fe7342b49922cc9673a3c7d10be921
Frnk29 <frcampoz@espol.edu.ec>
2026-08-09 18:11:59 -0500

    Inicio

---
+++ OneDrive/Documentos/NetBeansProjects/ED_P3_Grupo06/src/ed_p3_grupo06/ED_P3_Grupo06.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ed_p3_grupo06;
+
+/**
+ *
+ * @author fcamp
+ */
+public class ED_P3_Grupo06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
